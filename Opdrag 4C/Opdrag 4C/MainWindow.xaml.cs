d02ce2081d415aeec5fc990ae7c1e56badd798d0
Christiaan Koch <christiaankoch66@gmail.com>
2019-05-17 13:23:55 +0200

    Arrow keys now register input

--- Opdrag 4C/Opdrag 4C/MainWindow.xaml.cs
+++ Opdrag 4C/Opdrag 4C/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Opdrag_4C;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,8 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Forms;
-using System.Runtime.InteropServices;
+
 
 namespace Opdrag4C
 {
@@ -25,12 +25,12 @@ namespace Opdrag4C
     {
         public string pathName;
         public bool recording = false;
+        int counter = 0;
         private LowLevelKeyboardListener _listener;
 
         public MainWindow()
         {
             InitializeComponent();
-            NativeMethods.SetCursorPos(300, 300);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -45,39 +45,19 @@ namespace Opdrag4C
             pathName = dlg.FileName;
         }
 
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        internal static extern bool GetCursorPos(ref Win32Point pt);
-
-        [StructLayout(LayoutKind.Sequential)]
-        internal struct Win32Point
-        {
-            public Int32 X;
-            public Int32 Y;
-        };
-
-        public static Point GetMousePosition()
-        {
-            Win32Point w32Mouse = new Win32Point();
-            GetCursorPos(ref w32Mouse);
-            return new Point(w32Mouse.X, w32Mouse.Y);
-        }
-
-        public partial class NativeMethods
-        {
-            [System.Runtime.InteropServices.DllImportAttribute("user32.dll", EntryPoint = "SetCursorPos")]
-            [return: System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.Bool)]
-            public static extern bool SetCursorPos(int X, int Y);
-        }
-
         public void _listener_OnKeyPressed(Object sender, KeyPressedArgs e)
         {
             if ((e.KeyPressed == Key.LeftCtrl) && (e.KeyPressed == Key.LeftShift) && (e.KeyPressed == Key.R))
             {
                 if (recording)
+                {
                     recording = false;
+                }
                 else
+                {
                     recording = true;
+                }
+
             }
 
             if (recording)
@@ -96,27 +76,6 @@ namespace Opdrag4C
                                 writer.Write(e.KeyPressed.ToString().ToUpper());
                         }
                     }
-                }
-
-                Point mousePos = MainWindow.GetMousePosition();
-
-                switch (e.KeyPressed)
-                {
-                    
-                    case Key.Left:
-                        NativeMethods.SetCursorPos((int) mousePos.X - 10, (int) mousePos.Y);
-                        break;
-                    case Key.Right:
-                        NativeMethods.SetCursorPos((int) mousePos.X + 10, (int) mousePos.Y);
-                        break;
-                    case Key.Up:
-                        NativeMethods.SetCursorPos((int) mousePos.X, (int) mousePos.Y + 10);
-                        break;
-                    case Key.Down:
-                        NativeMethods.SetCursorPos((int) mousePos.X, (int) mousePos.Y - 10);
-                        break;
-                    default: 
-                        break;
                 }
             }
         }
